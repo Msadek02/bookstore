30872ca195d202fb7271189589375d2270c5bb48
Mostafa Sadek <thunder_axe_90@hotmail.com>
2016-08-22 16:09:42 -0400

    tag_list

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ActiveRecord::Base
   belongs_to :author
+  acts_as_taggable
+
+  def categories
+    self.tag_list
+  end
+
+  def self.find_by_category(category)
+    Book.tagged_with(category)
+  end
+
 end
