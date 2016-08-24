2889a0c977d86d4b9efb2e039f09c4c6ff985fe2
Mostafa Sadek <thunder_axe_90@hotmail.com>
2016-08-24 16:46:15 -0400

    removed testing line and show.html.erb

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,6 @@
 class HomesController < ApplicationController
   def index
     if params[:search].present?
-      puts "I made it in here"
       @books = Book.search(params[:search]).records
     end
   end
