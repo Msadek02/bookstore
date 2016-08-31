0e3d7b173283e6b1432ad536ac2bacf5c06dcba6
Mostafa Sadek <thunder_axe_90@hotmail.com>
2016-08-31 15:24:53 -0400

    sending email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,7 @@
 class OrderMailer < ApplicationMailer
+  def send_receipt(order_id)
+    @order = Order.find(order_id)
+    @user = @order.user
+    mail(to: @user.email, subject: 'Thanks you')
+  end
 end
