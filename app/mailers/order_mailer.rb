9ecf5482d9a0b10ca75c34874da114eae6fb671f
Mostafa Sadek <thunder_axe_90@hotmail.com>
2016-08-31 16:35:46 -0400

    adding quantity and book name and order_id when you send an email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,7 @@
 class OrderMailer < ApplicationMailer
   def send_receipt(order_id)
     @order = Order.find(order_id)
+    @order_items = @order.order_items
     @user = @order.user
     mail(to: @user.email, subject: 'Thanks you')
   end
