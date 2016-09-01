b428442ef327b1237fca6f04b5e8196f5954bb5a
Mostafa Sadek <thunder_axe_90@hotmail.com>
2016-09-01 12:39:47 -0400

    adding new email to authors

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831173256) do
+ActiveRecord::Schema.define(version: 20160901163807) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "avatar",     limit: 255
+    t.string   "email",      limit: 255
   end
 
   create_table "average_caches", force: :cascade do |t|
