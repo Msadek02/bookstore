e9c021b34d9c4e26361c853ac16b3632c5d91579
Msadek02 <Thunder_axe_90@hotmail.com>
2016-09-12 14:06:07 -0400

    adding new kaminari gem

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'ratyrate'
 gem 'acts_as_commentable'
 gem 'carrierwave'
 gem 'pry'
+gem 'kaminari'
 gem 'faker', '~> 1.6', '>= 1.6.3'
 gem "letter_opener", :group => :development
 gem 'elasticsearch-model', git: 'git://github.com/elasticsearch/elasticsearch-rails.git'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,27 @@
 Author.delete_all
-20.times do 
-	author = Author.create(name: Faker::Name.name,
-                           email: Faker::Internet.email,
-                           bio: Faker::Hipster.sentences
-		)
+Book.delete_all
+
+
+1.upto(20).each do |number|
+  author = Author.create( 
+				  	       id: number,
+						   name: Faker::Name.name, 
+						   email: Faker::Internet.email,
+						   bio: Faker::Hipster.sentences, 
+						   remote_avatar_url: Faker::Avatar.image
+						   )
 end
 
-Book.delete_all
-200.times do 
-	Book.create(name: Faker::Name.name, 
-		        picture: Faker::Avatar.image,
-                author_id: Faker::Book.author,
-                publish_date: Faker::Time.between(DateTime.now - 1, DateTime.now),
-                price: Faker::Commerce.price,
-                description: Faker::Lorem.sentence,
-                inventory: Faker::Number.between(1, 30),
-                minimum_age: Faker::Number.between(3, 9),
-                maximum_age: Faker::Number.between(10, 20)	
-		        )
+1.upto(200).each  do
+  Book.create(
+  	            name: Faker::Name.name, 
+			  	remote_picture_url: Faker::Avatar.image, 
+			  	author_id: rand(1..20), 
+			  	publish_date: Faker::Time.between(DateTime.now - 1, DateTime.now), 
+			  	price: Faker::Commerce.price, 
+			  	description: Faker::Lorem.sentence, 
+			  	inventory: Faker::Number.between(1, 30), 
+			  	minimum_age: Faker::Number.between(3, 9), 
+			  	maximum_age: Faker::Number.between(10, 20)
+  	)
 end
