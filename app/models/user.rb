16a59be13ec39abf5ec9e390930fe033e798fd08
Msadek02 <Thunder_axe_90@hotmail.com>
2016-09-20 15:16:05 -0400

    change avatar to image for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  mount_uploader :avatar, ImageUploader
+  mount_uploader :image, ImageUploader
   has_many :orders  
   has_many :comments
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,56 +1,57 @@
-Author.delete_all
-Comment.delete_all
-Book.delete_all
-OrderItem.delete_all
-Order.delete_all
-User.delete_all
-ActsAsTaggableOn::Tag.destroy_all
+# Author.delete_all
+# Comment.delete_all
+# Book.delete_all
+# OrderItem.delete_all
+# Order.delete_all
+# User.delete_all
+# ActsAsTaggableOn::Tag.destroy_all
 
-# created an empty array for store all the categories
-categories = []
+# # created an empty array for store all the categories
+# categories = []
 
-# Loop over 20 times and add random books to the categories array
-1.upto(20).each{ categories << Faker::Book.genre }
+# # Loop over 20 times and add random books to the categories array
+# 1.upto(20).each{ categories << Faker::Book.genre }
 
-1.upto(20).each do |number|
-  print '.'
-  Author.create(
-     id: number,
-     name: Faker::Name.name,
-     email: Faker::Internet.email,
-     bio: Faker::Hipster.sentences,
-     remote_avatar_url: Faker::Avatar.image
-  )
-end
+# 1.upto(20).each do |number|
+#   print '.'
+#   Author.create(
+#      id: number,
+#      name: Faker::Name.name,
+#      email: Faker::Internet.email,
+#      bio: Faker::Hipster.sentences,
+#      remote_avatar_url: Faker::Avatar.image
+#   )
+# end
 
 
-1.upto(200).each  do
-  print '.'
+# 1.upto(200).each  do
+#   print '.'
 
-  book = Book.create(
-    name: Faker::Name.name,
-    remote_picture_url: Faker::Avatar.image,
-    author_id: rand(1..20),
-    publish_date: Faker::Time.between(DateTime.now - 1, DateTime.now),
-    price: Faker::Commerce.price,
-    description: Faker::Lorem.sentence,
-    inventory: Faker::Number.between(1, 30),
-    minimum_age: Faker::Number.between(3, 9),
-    maximum_age: Faker::Number.between(10, 20),
-  )
+#   book = Book.create(
+#     name: Faker::Name.name,
+#     remote_picture_url: Faker::Avatar.image,
+#     author_id: rand(1..20),
+#     publish_date: Faker::Time.between(DateTime.now - 1, DateTime.now),
+#     price: Faker::Commerce.price,
+#     description: Faker::Lorem.sentence,
+#     inventory: Faker::Number.between(1, 30),
+#     minimum_age: Faker::Number.between(3, 9),
+#     maximum_age: Faker::Number.between(10, 20),
+#   )
 
-  random_category1 = categories[Faker::Number.between(0, 19)]
-  random_category2 = categories[Faker::Number.between(0, 19)]
-  book.tag_list.add(random_category1, random_category2)
-  book.save
-end
+#   random_category1 = categories[Faker::Number.between(0, 19)]
+#   random_category2 = categories[Faker::Number.between(0, 19)]
+#   book.tag_list.add(random_category1, random_category2)
+#   book.save
+# end
 
 1.upto(40).each do |number|
+  print '.'
   User.create(
     id: number,
     email: Faker::Internet.email,
     password: 'password',
     password_confirmation: 'password',
-    remote_picture_url: Faker::Avatar.image
+    remote_image_url: Faker::Avatar.image
   )
 end
