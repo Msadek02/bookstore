078513abf1f0342e020b5473df2eeb10fc5257a4
Msadek02 <Thunder_axe_90@hotmail.com>
2016-10-06 18:03:27 -0400

    login look, sorting categories maybe

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -2,6 +2,6 @@ class Rate < ActiveRecord::Base
   belongs_to :rater, :class_name => "User"
   belongs_to :rateable, :polymorphic => true
   belongs_to :book
-  #attr_accessible :rate, :dimension
+  #attr_attr_accessor :rate, :dimension
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
+  resources :comments
 
   post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
